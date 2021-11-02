26bdafca6b2d39d64411d386456e70c4fc1b34e2
Abdullokh <91453062+AbdullokhDev@users.noreply.github.com>
2021-11-02 15:58:18 +0500

    Add project files.

---
+++ MyFirstOOP/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstOOP
+{
+    class Person
+    {
+        // we are creating fields for person class
+        public string name;
+        public int age;
+        public string university;
+        public int level;
+
+        // creating method
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + name+"." + "I am  " + age + " years old");
+        }
+
+        public void abtEdu()
+        {
+            Console.WriteLine("My univerity is called" + " " + university + " " + "and my level is " + level);
+        }
+    }
+}
---
+++ MyFirstOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating real object oop language we called intance
+            Person student1 = new Person();
+            student1.name = "Abdullokh";
+            student1.age = 22;
+            student1.Introduce();
+            student1.university = "WIUT";
+            student1.level = 5;
+            student1.abtEdu();
+        }
+    }
+}
